201001c3c8a39f1a67e39a366ae9db02f3688592
GuiselleZP <guiselle2097@gmail.com>
2020-05-04 15:26:09 -0500

    Updated files

--- src/config/passport.js
+++ src/config/passport.js
@@ -4,16 +4,22 @@ const localStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
 
 passport.use("local-signin", new localStrategy({
-    usernameField: 'email1'
+    usernameField: "email1",
+    passwordField: "password",
+    passReqToCallback: true
 }, async(email1, password, done) =>{
+    console.log('ENTRA A SIGNIN');
+    console.log(email1, password);
     const user = await User.findOne({email1: email1});
     if(!user){
+        console.log("USUARIO NO EXISTE");
         return done(null, false, { message: "Not user founf" }, status = 400);
     } else {
         const match = await user.matchPassword(password);
         if(match) {
             return done(null, user, { message: "User logged in" , status: 200});
         } else {
+            console.log("CONTRASEÑA INCORRECTA");
             return done(null, false, {message : "Incorrect Password"}, status = 400);
         }
     }
@@ -33,6 +39,8 @@ passport.use("local-signup", new localStrategy({
 
     const newUser = new User(req.body);
     newUser.password =  await newUser.encryptPassword(password);
+    console.log("USUARIO GUARDADO");
+    console.log(newUser);
     await newUser.save();
     return done(null, newUser, { message: "Registered user", status: 201});
 }));
