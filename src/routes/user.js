1acde8c1197583a30dd098de015ea9004c4b5d93
GuiselleZP <guiselle2097@gmail.com>
2020-05-05 07:10:13 -0500

    update files

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,23 @@
 const { Router } = require("express");
 const router = Router();
 
+const {
+    updateUser,
+    deleteUser,
+    getUsers,
+    getUser
+} = require("../controllers/user.controller");
+
+
+router
+    .route("/")
+    .get(getUsers)
+  // .post(createUser);
+
+router
+    .route("/:id")
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
 module.exports = router;
