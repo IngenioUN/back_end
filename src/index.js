dc90936a9f9356ca6efb907a477e9289437ddc8d
Juan José Peña <jujpenabe@unal.edu.co>
2020-05-14 13:54:24 -0500

    Mmm no se que movi...

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
-
+// app.use('dev');
 app.use(cors());
 app.use(session({
     secret: 'IngenioUN',
