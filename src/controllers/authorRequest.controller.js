5d1a216a2eda27d34c616395756fb667fe0d694d
Juan José Peña <jujpenabe@unal.edu.co>
2020-06-01 15:20:23 -0500

    author request now works fine without autentification

--- src/controllers/authorRequest.controller.js
+++ src/controllers/authorRequest.controller.js
@@ -1,36 +1,30 @@
 const AuthorRequest = require('../models/AuthorRequest');
-const User = require('../models/User');
 const authorRequestCtrl = {};
-
+​
 // Juan
 authorRequestCtrl.addAuthorRequest = async (req, res) => {
     try{
-        if(req.user.role != 0){
-            return res.status(401).json({   // It has already author permission
-                message: "You do not have the required permissions"
-             });
-        }
-
-        req.body.userId = req.user.id;
-
+        /*
+        if(req.user.role != 0)
+            throw "You do not have the required permissions"
+        */
+        //req.body.userId = req.user.id;
+        req.body.userId = "5ec6b0256dee5c298d31cf52";
+​
         const Author = await AuthorRequest.findOne({ userId: req.body.userId});
-
-        if(Author){
-
-            return res.status(200).json({
-                message: "You have already send an author request"
-            });
-        }
-
+​
+        if(Author)
+            throw "You have already send an author request";
+​
         const { email2, professionalCard, employmentHistory, academicHistory } = req.body;
-
+​
         if(!email2 | !professionalCard | !employmentHistory | !academicHistory)
             throw "Incomplete data";
-
+​
         const newAuthorRequest = new AuthorRequest(req.body);
-
+​
         await newAuthorRequest.save();
-
+​
         return res.status(201).json({
             message: "The author request has been created successfully"
         });
@@ -43,42 +37,10 @@ authorRequestCtrl.addAuthorRequest = async (req, res) => {
             });
     }
 };
-
 // Valeria
-authorRequestCtrl.getAllAuthorRequest = async (req, res) => {
-    try {
-        
-        const authorRequest = await AuthorRequest.find().populate({path:'userId', select:['firstName','lastName']}).select('userId');
-        
-        return res.status(200).json(authorRequest)
-    } catch (err) {
-        if(!err.message)
-            return res.status(400).json({ message: err });
-        else
-            return res.status(400).json({
-                message: "Could not access"
-            });
-    }
-}
-authorRequestCtrl.getAuthorRequest = async (req, res) => {
-    try{
-        console.log(req.params);
-        const userId = req.params.userId;
-        //const { userId } = req.body;
-        
-        if (!userId)
-            throw "Incomplete data";
-        const authorRequest = await AuthorRequest.findOne({ userId: userId });
-        return res.status(200).json(authorRequest)
-    }catch(err){
-        return res.status(400).json({
-            message: "Could not access requested information"
-        })
-    }
-};
-
+​
 // Carlos
-
+​
 // Tatiana
-
+​
 module.exports = authorRequestCtrl;
