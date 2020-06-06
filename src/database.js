cd79211f502d0447e8420cf4c16a92ad6805a696
cdrinconm <cdrinconm@unal.edu.co>
2020-06-05 21:53:59 -0500

    BD sirviendo en MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.inv.MONGODB_URI || 'mongodb://localhost/ingenio_database', {
-//mongoose.connect('mongodb+srv://Ingenio123:Ingenio123@clusteringenio-jv1l1.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+//mongoose.connect(process.inv.MONGODB_URI || 'mongodb://localhost/ingenio_database', {
+mongoose.connect('mongodb+srv://Ingenio123:Ingenio123@clusteringenio-jv1l1.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority', {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
