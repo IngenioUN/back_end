0bb091acc57e99e56620808be55ac65740ef5949
GuiselleZP <guiselle2097@gmail.com>
2020-06-15 17:43:30 -0500

    CORS modified

--- src/app.js
+++ src/app.js
@@ -9,13 +9,26 @@ const path = require('path');   // Manage directory path
 require('./config/passport');
 const app = express();
 
+
+app.use(cors({
+    origin: "http://localhost:8080",
+    credentials: true
+}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+
 // Settings
 app.set('port', process.env.PORT || 3000);
 
 // setup the logger
 var accessLogStream = rfs.createStream('access.log', {
-    interval: '1d',
-    path: path.join(__dirname, 'log')
+interval: '1d',
+path: path.join(__dirname, 'log')
  });
 
 app.use(morgan('combined', { stream: accessLogStream }))
@@ -23,7 +36,6 @@ app.use(morgan('combined', { stream: accessLogStream }))
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors());
 app.use(session({
     secret: 'IngenioUN',
     resave: true,
@@ -31,14 +43,6 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin,\
-    X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
 
 // Routes
 
