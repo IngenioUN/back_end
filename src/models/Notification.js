2479e4e2374b16d2866cd4caa3adea46faee8f0d
ValeHuDU <valeri.hudu@gmail.com>
2020-06-19 20:27:19 -0500

    update models and routes

---
+++ src/models/Notification.js
@@ -0,0 +1,10 @@
+const mongoose = require( 'mongoose' );
+const { Schema } = mongoose;
+
+const Notification = new Schema({
+    authorId : { type: Schema.Types.ObjectId, ref: 'User' },
+    categoryId : { type: Schema.Types.ObjectId, ref: 'Category' },
+    userId : { type: Schema.Types.ObjectId, ref: 'User' },
+    listPublication : { type: Schema.Types.ObjectId, ref: 'Publication' },
+})
+module.exports = mongoose.model( 'Notification', Notification );
