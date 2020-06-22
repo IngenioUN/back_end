ff7d66cd8a3c662eb879b3417cebee1d923cb13f
GuiselleZP <guiselle2097@gmail.com>
2020-06-22 01:37:21 -0500

    removed unnecessary lines

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -17,7 +17,6 @@ notificationCtrl.subscribe = async ( req, res ) => {
         if ( req.user.role == 2 )
             throw "You do not have the required permissions";
 
-        console.log(req.body );
         if ( req.body.categoryId )
             notification = await Notification.findOne({ categoryId: req.body.categoryId });
         else if ( req.body.authorId )
@@ -28,11 +27,9 @@ notificationCtrl.subscribe = async ( req, res ) => {
         if ( !notification ) {
             const newNotification = new Notification( req.body );
             newNotification.subscribers.push( req.user.id );
-            console.log(newNotification);
             await newNotification.save();
         } else {
             notification.subscribers.push( req.user.id );
-            console.log(notification);
             await Notification.findByIdAndUpdate( notification.id, notification );
         }
 
@@ -49,7 +46,7 @@ notificationCtrl.subscribe = async ( req, res ) => {
             logger.warn( err );
             return res.status( 400 ).json({ message: err });
         } else {
-        logger.error( "there is a problem registering subscription notifications" );
+            logger.error( "there is a problem registering subscription notifications" );
             return res.status( 400 ).json({
                 message: "There was a problem with the requested subscription"
             })
@@ -69,7 +66,6 @@ notificationCtrl.unsubscribe = async ( req, res ) => {
             notification = await Notification.findOne({ authorId: req.body.authorId });
         else
             throw "Incomplete data"
-        console.log(notification);
         if ( notification ) {
             for ( var i = 0; i < notification.subscribers.length; i++ ) {
                 if ( notification.subscribers[ i ] == req.user.id ) {
@@ -85,23 +81,23 @@ notificationCtrl.unsubscribe = async ( req, res ) => {
                 throw "You are not subscribed to this author"
         }
 
+        if ( req.body.authorId )
+            logger.info( "User unsubscribed to an author" )
         if( req.body.categoryId )
             logger.info( "User unsubscribed to a category" )
-        else if ( req.body.authorId )
-            logger.info( "User unsubscribed to an author" )
 
         return res.status( 200 ).json({
             message: "The unsubscription has been successful"
         })
     } catch ( err ) {
-        if( !err.message ){
-            logger.warn( err );
-            return res.status( 400 ).json({ message: err });
-        } else {
-        logger.error( "There is a problem deleting the subscription" );
+        if( err.message ){
+            logger.error( "There is a problem deleting the subscription" );
             return res.status( 400 ).json({
                 message: "There was a problem with the requested unsubscription"
             })
+        } else {
+            logger.warn( err );
+            return res.status( 400 ).json({ message: err });
         }
     }
 }
