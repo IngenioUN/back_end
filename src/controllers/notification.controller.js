5d47ffeb64edec9a32ac59d9bc71f734f7e30d0e
GuiselleZP <guiselle2097@gmail.com>
2020-06-22 02:20:54 -0500

    Remove subscriptors from Notification model

--- src/controllers/notification.controller.js
+++ src/controllers/notification.controller.js
@@ -17,26 +17,16 @@ notificationCtrl.subscribe = async ( req, res ) => {
         if ( req.user.role == 2 )
             throw "You do not have the required permissions";
 
-        if ( req.body.categoryId )
-            notification = await Notification.findOne({ categoryId: req.body.categoryId });
-        else if ( req.body.authorId )
-            notification = await Notification.findOne({ authorId: req.body.authorId });
-        else
-            throw "Incomplete data"
-
-        if ( !notification ) {
-            const newNotification = new Notification( req.body );
-            newNotification.subscribers.push( req.user.id );
-            await newNotification.save();
-        } else {
-            notification.subscribers.push( req.user.id );
-            await Notification.findByIdAndUpdate( notification.id, notification );
-        }
+        const newNotification = new Notification( req.body );
+        newNotification.userId = req.user.id;
+        await newNotification.save();
 
         if( req.body.categoryId )
             logger.info( "User subscribed to a category" )
         else if ( req.body.authorId )
             logger.info( "User subscribed to an author" )
+        else
+            throw "Incomplete data"
 
         return res.status( 200 ).json({
             message: "The subscription has been successful"
@@ -56,30 +46,21 @@ notificationCtrl.subscribe = async ( req, res ) => {
 
 notificationCtrl.unsubscribe = async ( req, res ) => {
     try {
-        var notification;
         if ( req.user.role == 2 )
             throw "You do not have the required permissions";
 
         if ( req.body.categoryId )
-            notification = await Notification.findOne({ categoryId: req.body.categoryId });
+            await Notification.findOneAndRemove({
+                categoryId: req.body.categoryId,
+                userId: req.user.id
+            });
         else if ( req.body.authorId )
-            notification = await Notification.findOne({ authorId: req.body.authorId });
+            await Notification.findOneAndRemove({
+                authorId: req.body.authorId,
+                userId: req.user.id
+            });
         else
             throw "Incomplete data"
-        if ( notification ) {
-            for ( var i = 0; i < notification.subscribers.length; i++ ) {
-                if ( notification.subscribers[ i ] == req.user.id ) {
-                    notification.subscribers.splice( i, 1 );
-                    break;
-                }
-            }
-            await Notification.findByIdAndUpdate( notification.id, notification );
-        } else {
-            if ( req.body.categoryId )
-                throw "You are not subscribed to this category"
-            else if ( req.body.authorId )
-                throw "You are not subscribed to this author"
-        }
 
         if ( req.body.authorId )
             logger.info( "User unsubscribed to an author" )
@@ -90,10 +71,15 @@ notificationCtrl.unsubscribe = async ( req, res ) => {
             message: "The unsubscription has been successful"
         })
     } catch ( err ) {
+        var message;
         if( err.message ){
-            logger.error( "There is a problem deleting the subscription" );
+            if ( req.body.categoryId )
+                message = "You are not subscribed to this category"
+            else
+                message = "You are not subscribed to this author"
+            logger.error( "User entered an invalid subscription id" );
             return res.status( 400 ).json({
-                message: "There was a problem with the requested unsubscription"
+                message: message
             })
         } else {
             logger.warn( err );
